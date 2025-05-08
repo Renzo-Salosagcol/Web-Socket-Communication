1bde1802c2d9642445f9661ac74c9a02d93e160c
eemont <emont@csu.fullerton.edu>
2025-05-07 20:51:06 -0700

    Fixed dbname typo

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'sql109.infinity.com',      // replace with your actual host
+    host: 'sql109.infinityfree.com',      // replace with your actual host
     user: 'if0_38874660',         // replace with your InfinityFree username
     password: 'vF6Q11hUHg5jHbF',      // replace with your InfinityFree password
     database: 'if0_38874660_yap_data'   // replace with your database name
